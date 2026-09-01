8aa2acf4df4bf23d014c9a97e0c7fda7f6ffb23c
PAVAN KUMAR PV <92670410+pvpavankumar@users.noreply.github.com>
2026-09-01 17:04:27 +0530

    Add résumé download icon

---
+++ src/components/svgs/download.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function Download({ className }) {
+    return <svg className={className} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 16a1 1 0 0 1-.71-.29l-4-4a1 1 0 0 1 1.42-1.42L11 12.59V3a1 1 0 0 1 2 0v9.59l2.29-2.3a1 1 0 0 1 1.42 1.42l-4 4A1 1 0 0 1 12 16zM20 21H4a3 3 0 0 1-3-3v-2a1 1 0 0 1 2 0v2a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1v-2a1 1 0 0 1 2 0v2a3 3 0 0 1-3 3z" /></svg>;
+}
