b525982c5b955724410d134b54ba0f42823ec1bb
PAVAN KUMAR PV <92670410+pvpavankumar@users.noreply.github.com>
2026-09-01 17:04:35 +0530

    Register professional navigation icons

--- src/components/svgs/index.js
+++ src/components/svgs/index.js
@@ -6,20 +6,24 @@ import ArrowRightCircle from './arrow-right-circle';
 import ArrowUpLeft from './arrow-up-left';
 import ArrowUpRight from './arrow-up-right';
 import Bluesky from './bluesky';
+import Briefcase from './briefcase';
 import Cart from './cart';
 import ChevronLeft from './chevron-left';
 import ChevronRight from './chevron-right';
 import Facebook from './facebook';
+import Download from './download';
 import GitHub from './github';
 import GooglePlay from './google-play';
 import Instagram from './instagram';
 import LinkedIn from './linkedin';
 import Mail from './mail';
+import MapPin from './map-pin';
 import Play from './play';
 import PlayCircle from './play-circle';
 import Reddit from './reddit';
 import Send from './send';
 import Twitter from './twitter';
+import User from './user';
 import Vimeo from './vimeo';
 import YouTube from './youtube';
 
@@ -32,20 +36,24 @@ export const iconMap = {
     arrowUpLeft: ArrowUpLeft,
     arrowUpRight: ArrowUpRight,
     bluesky: Bluesky,
+    briefcase: Briefcase,
     cart: Cart,
     chevronLeft: ChevronLeft,
     chevronRight: ChevronRight,
     facebook: Facebook,
+    download: Download,
     github: GitHub,
     googlePlay: GooglePlay,
     instagram: Instagram,
     linkedin: LinkedIn,
     mail: Mail,
+    mapPin: MapPin,
     play: Play,
     playCircle: PlayCircle,
     reddit: Reddit,
     send: Send,
     twitter: Twitter,
+    user: User,
     vimeo: Vimeo,
     youtube: YouTube
 };
