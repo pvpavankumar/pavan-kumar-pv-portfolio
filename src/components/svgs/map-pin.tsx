4146e1e514b25e7223150de49b19a5f522c5d383
PAVAN KUMAR PV <92670410+pvpavankumar@users.noreply.github.com>
2026-09-01 17:04:31 +0530

    Add location contact icon

---
+++ src/components/svgs/map-pin.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function MapPin({ className }) {
+    return <svg className={className} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 1a8 8 0 0 0-8 8c0 5.73 6.82 13.2 7.11 13.52a1.2 1.2 0 0 0 1.78 0C13.18 22.2 20 14.73 20 9a8 8 0 0 0-8-8zm0 19.27C10.1 18.04 6 12.78 6 9a6 6 0 0 1 12 0c0 3.78-4.1 9.04-6 11.27zM12 5a4 4 0 1 0 0 8 4 4 0 0 0 0-8zm0 6a2 2 0 1 1 0-4 2 2 0 0 1 0 4z" /></svg>;
+}
