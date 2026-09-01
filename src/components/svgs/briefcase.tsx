b16fe07b60a4b749426932fc4654eff4da7b241d
PAVAN KUMAR PV <92670410+pvpavankumar@users.noreply.github.com>
2026-09-01 17:04:24 +0530

    Add portfolio navigation icon

---
+++ src/components/svgs/briefcase.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function Briefcase({ className }) {
+    return <svg className={className} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M21 6h-4V4.5A2.5 2.5 0 0 0 14.5 2h-5A2.5 2.5 0 0 0 7 4.5V6H3a2 2 0 0 0-2 2v11a2 2 0 0 0 2 2h18a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2zM9 4.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 .5.5V6H9V4.5zM21 8v4.18A15.1 15.1 0 0 1 13 14.9V13a1 1 0 0 0-2 0v1.9A15.1 15.1 0 0 1 3 12.18V8h18zM3 19v-4.51A17.1 17.1 0 0 0 11 16.9V18a1 1 0 0 0 2 0v-1.1a17.1 17.1 0 0 0 8-2.41V19H3z" /></svg>;
+}
