db73cac73fea484fa6ae39898d023de06bdee597
PAVAN KUMAR PV <92670410+pvpavankumar@users.noreply.github.com>
2026-09-01 17:04:20 +0530

    Add profile navigation icon

---
+++ src/components/svgs/user.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function User({ className }) {
+    return <svg className={className} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 12c3.04 0 5.5-2.46 5.5-5.5S15.04 1 12 1 6.5 3.46 6.5 6.5 8.96 12 12 12zm0-9c1.93 0 3.5 1.57 3.5 3.5S13.93 10 12 10 8.5 8.43 8.5 6.5 10.07 3 12 3zm0 11c-5.05 0-9 2.64-9 6v2a1 1 0 0 0 2 0v-2c0-1.63 2.73-4 7-4s7 2.37 7 4v2a1 1 0 0 0 2 0v-2c0-3.36-3.95-6-9-6z" /></svg>;
+}
